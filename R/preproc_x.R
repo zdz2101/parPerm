edc0f104235a67fe526d5d0fcbb22a14e1f4d70b
Zelos Zhu <zdz2101@cumc.columbia.edu>
2019-08-02 08:22:11 -0400

    use pkgdown

--- R/preproc_x.R
+++ R/preproc_x.R
@@ -2,7 +2,7 @@
 #depends how "clean the characteristic table of x comes in as, will need to figure out how to identify columns with variables
 
 xmat_trans <- function(xmat) {
-  xmat=as.matrix(xmat)
-  xtx=xmat %*% solve(t(xmat) %*% xmat)
+  xmat = as.matrix(xmat)
+  xtx = xmat %*% solve(t(xmat) %*% xmat)
   return(xtx)
 }
--- R/preproc_y.R
+++ R/preproc_y.R
@@ -1,21 +1,40 @@
 #Problems: what is mask == 1 for>
-ymat_trans <- function(gmvol, mask){
+ymat_trans <- function(gmvol, mask, mask_logical = FALSE){
 
-  if(class(gmvol)[1] != "nifti" | class(mask)[1] != "nifti"){
-     print("Your files are not nifti files.")
+  if (mask_logical == TRUE){
+    
+    #check to see if files are NIFTI
+    if(class(gmvol)[1] != "nifti" | class(mask)[1] != "nifti"){
+      print("Your files are not nifti files.")
+    }
+    
+    else{
+      
+      #Get the dimensions your data
+      dim = c( prod(dim(gmvol)[1:3]), #-- product of the 3 imaging dimensions
+               dim(gmvol)[4] # -- num of obs/patients
+      )
+      
+      #Use the mask of 1 -- ask about this
+      ymat = array(gmvol@.Data, dim)[mask@.Data==1,]
+    }
+    return(ymat)
   }
-
+  
   else{
-
-  #Get the dimensions your data
-  dim = c( prod(dim(gmvol)[1:3]), #-- product of the 3 imaging dimensions
-           dim(gmvol)[4] # -- num of obs/patients
-          )
-
-  #Use the mask of 1 -- ask about this
-  ymat = array(gmvol@.Data, dim)[mask@.Data==1,]
+    if(class(gmvol)[1] != "nifti"){
+      print("Your files are not nifti files.")
+    }
+    
+    else{
+      dim = c( prod(dim(gmvol)[1:3]), #-- product of the 3 imaging dimensions
+               dim(gmvol)[4] # -- num of obs/patients
+      )
+      
+      #Use the mask of 1 -- ask about this
+      ymat = array(gmvol@.Data, dim)
+    }
+    return(ymat)
   }
-
-  return(ymat)
 }
 
